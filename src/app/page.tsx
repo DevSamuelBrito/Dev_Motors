aa3924274e9f995ba5d19f5a434a828acdab9f07
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-01-06 22:48:24 -0300

    criando o header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-
 export default function Home() {
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>      
+    <main>
+      
+    </main>
   );
 }
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,9 +1,39 @@
+"use client";
+
+import Link from "next/link";
+import { useState, useEffect } from "react";
+import styles from "./styles.module.scss";
+
 const Header = () => {
-    return (
-        <div>
-            teste header
+  const [top, setTop] = useState(true);
+
+  function scrollHandler() {
+    window.scrollY > 10 ? setTop(false) : setTop(true);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollHandler);
+    return () => {
+      window.removeEventListener("scroll", scrollHandler);
+    };
+  }, [top]);
+
+  return (
+    <header className={`${styles.header} ${!top ? styles.fixed : styles.background}`}>
+      <div className={styles.container}>
+        <div className={styles.content}>
+          <div className={styles.contentLogo}>
+            <Link href="/">Dev Motors </Link>
+          </div>
+          <nav className={styles.contentNav}>
+            <Link href="/">HOME</Link>
+            <Link href="/#serviços">SERVIÇOS</Link>
+            <Link href="/contatos">CONTATOS</Link>
+          </nav>
         </div>
-    );
-}
+      </div>
+    </header>
+  );
+};
 
 export default Header;
