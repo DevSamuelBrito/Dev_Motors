c3b6b5b610e0afddc3e3eac400d04b8927689777
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-01-16 11:42:29 -0300

    terminando de criar o footer

--- src/components/home/footer/index.tsx
+++ src/components/home/footer/index.tsx
@@ -1,15 +1,40 @@
-"use client"
+"use client";
 import { HomeProps } from "@/utils/home.types";
-import styles from "./styles.module.scss"
+import styles from "./styles.module.scss";
+import { Clock, Mail, Map, Phone } from "lucide-react";
 
-const Footer = ({object}:HomeProps) => {
-    return (
-        <footer id="contatos" className={styles.footer}>
-            <section className={styles.section}>
-
-            </section>
-        </footer>
-    );
-}
+const Footer = ({ object }: HomeProps) => {
+  return (
+    <footer id="contatos" className={styles.footer}>
+      <section className={styles.section}>
+        <h2 className={styles.title}>Contatos</h2>
+        <div className={styles.content}>
+          <div className={styles.item}>
+            <Mail size={24} color="#FFF" />
+            <div>
+              <strong>Email</strong>
+              <p>{object.metadata.contact.email}</p>
+            </div>
+            <Phone size={24} color="#FFF" />
+            <div>
+              <strong>Telefone</strong>
+              <p>{object.metadata.contact.phone}</p>
+            </div>
+            <Map size={24} color="#FFF" />
+            <div>
+              <strong>Endereço</strong>
+              <p>{object.metadata.contact.address}</p>
+            </div>
+            <Clock size={24} color="#FFF" />
+            <div>
+              <strong>Horário</strong>
+              <p>{object.metadata.contact.time}</p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </footer>
+  );
+};
 
 export default Footer;
