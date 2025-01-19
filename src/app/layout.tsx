7cc1ae6e456a0d47dae754364c0e6a0235092441
DevSamuelBrito <samuelbrito.dev@gmail.com>
2025-01-19 17:25:12 -0300

    criando metadatas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,29 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.scss";
-import Header from "@/components/header";
+import Header from "../components/header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "DevMotors - Oficina Especializada",
-  description: "Oficina especializada em manutenção de veículos",
+  title: "DevMotors - Sua oficina especializada!",
+  description: "Oficina de carros em São Paulo",
+  keywords: ["oficina", "oficina carros", "carros", "manutenção de carros"],
+  openGraph: {
+    title: "DevMotors - Sua oficina especializada!",
+    images: [`${process.env.NEXT_PUBLIC_URL}/logo.jpg`]
+  },
+  robots:{
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+    }
+  }
 };
 
 export default function RootLayout({
@@ -25,10 +33,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Header />
+      <body className={inter.className}>
+        <Header/>
         {children}
         <p style={{ textAlign: "center", marginTop: 54, marginBottom: 24 }}>
           Todos direitos reservados @{`${new Date().getFullYear()}`}
